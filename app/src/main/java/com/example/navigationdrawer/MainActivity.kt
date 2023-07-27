a4351b022af1c526b2dd5b99c896d5806b707a11
FATIQ HUSSNAIN <fatiqhussnain1@gmail.com>
2023-07-27 10:16:39 -0700

    Navigation Drawer App using themes and other components

--- app/src/main/java/com/example/navigationdrawer/MainActivity.kt
+++ app/src/main/java/com/example/navigationdrawer/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.navigationdrawer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var drawer: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        drawer = findViewById(R.id.drawer_layout)
+
+        val toogle = ActionBarDrawerToggle(
+            this,
+            drawer,
+            R.string.open,
+            R.string.close
+        )
+
+        drawer.addDrawerListener(toogle)
+
+        toogle.syncState()
     }
 }
