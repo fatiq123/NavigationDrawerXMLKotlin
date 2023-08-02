e5bb8b59a9a49c037e12a031926f7aa052ab03b6
FATIQ HUSSNAIN <fatiqhussnain1@gmail.com>
2023-08-01 23:24:37 -0700

    working with Navigation drawer and fragments

--- app/src/main/java/com/example/navigationdrawer/MainActivity.kt
+++ app/src/main/java/com/example/navigationdrawer/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.navigationdrawer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import com.example.navigationdrawer.fragments.ChatFragment
+import com.example.navigationdrawer.fragments.ProfileFragment
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,6 +24,7 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         drawer = findViewById(R.id.drawer_layout)
+        val navigationView = findViewById<NavigationView>(R.id.nav_view)
 
         val toggle = ActionBarDrawerToggle(
             this@MainActivity,
@@ -30,6 +36,54 @@ class MainActivity : AppCompatActivity() {
         drawer.addDrawerListener(toggle)
 
         toggle.syncState()
+
+
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container, ProfileFragment())
+            .commit()
+        navigationView.setCheckedItem(R.id.miProfile)
+
+
+
+
+        navigationView.setNavigationItemSelectedListener(object : NavigationView.OnNavigationItemSelectedListener{
+            override fun onNavigationItemSelected(item: MenuItem): Boolean {
+                when(item.itemId) {
+                    R.id.miProfile -> {
+                        supportFragmentManager
+                            .beginTransaction()
+                            .replace(R.id.fragment_container, ProfileFragment())
+                            .commit()
+                    }
+                    R.id.miChat -> {
+                        supportFragmentManager
+                            .beginTransaction()
+                            .replace(R.id.fragment_container, ChatFragment())
+                            .commit()
+                    }
+                    R.id.miSend -> {
+                        Toast.makeText(
+                            this@MainActivity,
+                            "Send Selected",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                    R.id.miShare -> {
+                        Toast.makeText(
+                            this@MainActivity,
+                            "Share Selected",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+                drawer.closeDrawer(GravityCompat.START)
+                return true
+            }
+
+        })
+
     }
 
     override fun onBackPressed() {
